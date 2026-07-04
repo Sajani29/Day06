3ec440c4b3d23cdc8772c3beee415ffb67fd5d74
Sajani <22cis0309@ms.sab.ac.lk>
2026-07-04 09:56:53 +0530

    add object

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 console.log("hello day 06");
+
+let Customer1 = {
+    name: "John Doe",
+    age: 30,
+    email: "mial@.com"
+};
+console.log(Customer1.name);
+console.log(Customer1.age); 
+console.log(Customer1.email);
